9b0597b7aafbafcb292aebe31b93bfd4f0d8a3d2
Carlos Porras <porrasc25@gmail.com>
2015-10-14 17:05:31 -0500

    se agrego escala y mejor css

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,5 @@
 class Position < ActiveRecord::Base
+  has_many :employees
   validates :nombre , presence: { message: " es requerido"}  , confirmation: true
   validates :sueldo , presence: { message: " es requerido"}  , confirmation: true
   validates :sueldo, numericality: { only_integer: true ,message: "no es un numero"}
