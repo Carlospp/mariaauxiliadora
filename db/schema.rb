4015bcf701aa5c8afdde7803983631d13672e467
Carlos Porras <porrasc25@gmail.com>
2015-10-18 00:20:14 -0500

    se agrego usuarios x defecto en seed y se quito los errores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017033747) do
+ActiveRecord::Schema.define(version: 20151018043915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,19 +131,19 @@ ActiveRecord::Schema.define(version: 20151017033747) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "rol"
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "rol",                    default: "usuario"
     t.string   "name"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(:email => 'porrasc25@gmail.com', 
               :password => 'carlos23', 
-              :password_confirmation => 'carlos23')
+              :password_confirmation => 'carlos23',
+              :name => 'Carlos Porras',
+              :rol => 'admin')
+
+User.create!(:email => 'mnarro50@hotmail.com', 
+              :password => 'unodostres', 
+              :password_confirmation => 'unodostres',
+              :name => 'Ing. Manuel Narro',
+              :rol => 'admin')
+
+User.create!(:email => 'hebmartinez@hotmail.com', 
+              :password => 'unodostres', 
+              :password_confirmation => 'unodostres',
+              :name => 'Dr. Herbert Martinez',
+              :rol => 'admin')
+
+User.create!(:email => 'hmartinez@gmail.com', 
+              :password => 'unodostres', 
+              :password_confirmation => 'unodostres',
+              :name => 'Dr. Herbert Martinez',
+              :rol => 'odontologo')
+
+User.create!(:email => 'secretaria@gmail.com', 
+              :password => 'unodostres', 
+              :password_confirmation => 'unodostres',
+              :name => 'Srta. Secretaria',
+              :rol => 'secretaria')
